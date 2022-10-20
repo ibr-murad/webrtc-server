ff170e10274f731c26420702d51495227ba01346
Murad Ibrohimov <>
2022-10-20 14:39:46 +0400

    fix

--- server.js
+++ server.js
@@ -174,13 +174,13 @@ io.on('connection', socket => {
     
 });
 
-//const publicPath = path.join(__dirname, 'build');
-//
-//app.use(express.static(publicPath));
-//
-//app.get('*', (req, res) => {
-//    res.sendFile(path.join(publicPath, 'index.html'));
-//});
+const publicPath = path.join(__dirname, 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 server.listen(3000, () => {
     console.log('Server Started!')
