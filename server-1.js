d4fa0a967fc0ef115c4c7a2696eddc2c3a21299d
Murad Ibrohimov <>
2022-10-20 15:20:49 +0400

    fix

--- server-1.js
+++ server-1.js
@@ -4,10 +4,16 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
 
-app.listen(PORT, () => console.log('Example app listening'))
+server.listen(PORT, () => {
+    console.log('Server Started!')
+})
+
+//app.listen(PORT, () => console.log('Example app listening'))
 
 io.on('connection', socket => {
     console.log('connected to the server');
 });
+
+
+app.get('/', (req, res) => res.send('Hello World!!!'));
